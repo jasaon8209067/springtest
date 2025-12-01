ae0901a03d0c920b7b7fb8d1ced7dcf2286fcffd
jaystar0805 <jasonqwe1234@gmail.com>
2025-12-01 15:58:29 +0800

    刪除不再使用的事件排程和庫存控制器及相關實體，簡化代碼結構

--- src/main/java/tw/jay/springtest/repository/ReservationItemRepo.java
+++
@@ -1,9 +0,0 @@
-package tw.jay.springtest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tw.jay.springtest.entity.ReservationItem;
-
-public interface ReservationItemRepo extends JpaRepository<ReservationItem, Long> {
-    
-}
--- src/main/java/tw/jay/springtest/repository/ReservationRepo.java
+++
@@ -1,9 +0,0 @@
-package tw.jay.springtest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tw.jay.springtest.entity.Reservation;
-
-public interface ReservationRepo extends JpaRepository<Reservation, Long> {
-    
-}
